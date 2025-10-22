700e47918bebd56b14a0d5396c0f3feadb8952e7
zaina33 <zaina33@nycstudents.net>
2025-10-22 12:45:06 +0000

    aa

--- VR_Unit2/VRPractical2_2/script.js
+++ VR_Unit2/VRPractical2_2/script.js
@@ -8,10 +8,12 @@ window.addEventListener("DOMContentLoaded",function() {
     box = document.querySelector("#car");
     sphere = document.querySelector("#pokemonball");
     cone = document.querySelector("#rocket");
+    dude = document.querySelector("#dude");
+    sun = document.querySelector("#sun");
     box.a = 0.01;
-    box.da = .1;
-    sphere.a =0.5;
-    sphere.da = .5;
+    box.da = .01;
+    sphere.a =0.01;
+    sphere.da = .1;
     loop();
   })
 
@@ -21,6 +23,7 @@ window.addEventListener("DOMContentLoaded",function() {
         box.setAttribute("position",{x:0,y:0,z:box.a});
         sphere.setAttribute("rotation",{x:sphere.a,y:0,z:0});
         cone.setAttribute("position", {x:0,y:sphere.a,z:0});
+        dude.setAttribute("scale", {x:box.a,y:box.a,z:box.a});
         window.requestAnimationFrame(loop);
     }
 
