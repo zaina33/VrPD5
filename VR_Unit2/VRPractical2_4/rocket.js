f5c416730f12ffe6880f67990279787852758ff1
zaina33 <zaina33@nycstudents.net>
2025-10-28 12:25:05 +0000

    asdad

--- VR_Unit2/VRPractical2_4/rocket.js
+++ VR_Unit2/VRPractical2_4/rocket.js
@@ -11,9 +11,19 @@ class Rocket{
 
     let rocket2 = document.createElement("a-cylinder");
     rocket2.setAttribute("color","grey");
-    rocket2.setAttribute("position","0 0 0");
+    rocket2.setAttribute("position","0 -.2 0");
+    rocket2.setAttribute("height","1.4")
     rocket2.setAttribute("radius",".6");
     this.obj.append( rocket2 );
+
+    let rocket3 = document.createElement("a-cone");
+    rocket3.setAttribute("color","orange");
+    rocket3.setAttribute("position","0 -1 0");
+    rocket3.setAttribute("height","2");
+    rocket3.setAttribute("scale",".5 .7 .5");
+    rocket3.setAttribute("rotation","180 0 0");
+    this.obj.append( rocket3 );
+    
 
   
     this.obj.setAttribute("position",{x:x, y:y, z:z});
--- VR_Unit2/VRPractical2_4/script.js
+++ VR_Unit2/VRPractical2_4/script.js
@@ -1,5 +1,6 @@
 let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
 let scene;
+let box;
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene"); 
@@ -8,12 +9,20 @@ window.addEventListener("DOMContentLoaded",function() {
     let x = rnd(-50,30);
     let z = rnd(-50,30);
     let rocket = new Rocket(x,1,z);
+
   }
+    box = document.querySelector("#rocket");
+
+    box.a = 0.1;
+    box.da = .01;
+
 
   loop();
 })
 
 function loop(){
+        box.a+= box.da;
+        box.setAttribute("position", {x:0,y:1,z:0});
 
   
   window.requestAnimationFrame( loop );
